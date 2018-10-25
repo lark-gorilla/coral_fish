98c4a2aaf8d4f39b94891735387a298bace16724
Mark Miller <fbsmmi@leeds.ac.uk>
2018-10-25 12:58:38 +0100

    Updates to shiny app to include all regions. Need to change checkboxInput to group input to fix issue with only >1 value bing allowed

--- Dynamic_trait_explore/server.R
+++ Dynamic_trait_explore/server.R
@@ -9,14 +9,12 @@
 
 library(shiny)
 library(dplyr)
-library(readxl)
 library(ggplot2)
 #https://daijiang.name/en/2014/05/11/functional-diversity-in-r/
 library(ade4)
 library(cluster)
 library(factoextra)
 library(FD)
-library(shiny)
 
 
 gg_color_hue <- function(n) {
@@ -25,54 +23,33 @@ gg_color_hue <- function(n) {
   
 # read in coral and fish abundance and trait data
 
-fishdat<-read_excel('~/leeds_postdoc/data/RMI/RMIfish_siteDepthMeans_ADepth.xlsx', sheet=1)
-
-fishtrait<-read.csv('~/leeds_postdoc/data/Traits/RMI_fish_traits4.csv', h=T)
-
-#test names line up
-
-n1<-names(fishdat)[-1] # without site col name
-n2<-fishtrait$Species
-
-# FYI more species in trait dataset than abundance dataset
-
-ingrid<-expand.grid(n1, n2)
-ingrid$Var1<-as.character(ingrid$Var1)
-ingrid$Var2<-as.character(ingrid$Var2)
-
-ingrid$test=ingrid$Var1==ingrid$Var2
-
-ingrid_out<- ingrid %>% group_by(Var1) %>%
-  summarise(has_trait=TRUE%in%test)
-
-#filter(ingrid_out, has_trait==F)
-# only Bryanops natans
-# called Bryaninops natans in trait data - correct
-names(fishdat)[names(fishdat)=='Bryanops natans']<-'Bryaninops natans'
-
-fishtrait2<-filter(fishtrait, Species%in% names(fishdat))
-
-row.names(fishtrait2)<-fishtrait2$Species
+fishtrait<-read.csv('./data/JPN_AUS_RMI_trait_master.csv', h=T)
+row.names(fishtrait)<-fishtrait$Species
+fishtrait$Species<-NULL
 
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
    
   mydata=reactive({
     selnames=NULL
-    if(input$HomeRange){selnames=c(selnames, 1)}
-    if(input$MaxLength){selnames=c(selnames, 2)}
-    if(input$PLD_Mean){selnames=c(selnames, 3)}
-    if(input$TrophLevel){selnames=c(selnames, 4)}
-    if(input$Food){selnames=c(selnames, 5)}
-    if(input$Function){selnames=c(selnames, 6)}
-    if(input$Aggregation){selnames=c(selnames, 7)}
-    if(input$Position){selnames=c(selnames, 8)}
-    if(input$SpawnMode){selnames=c(selnames, 9)}
-    if(input$ParentalMode){selnames=c(selnames, 10)}
-    if(input$Active){selnames=c(selnames, 11)}
+    seldat=NULL
     
-    md=fishtrait2[,selnames]
+    if(input$JPN){seldat=c(seldat, 9)}
+    if(input$AUS){seldat=c(seldat, 10)}
+    if(input$RMI){seldat=c(seldat, 11)}
+    
+    if(input$ThermalAffinity){selnames=c(selnames, 1)}
+    if(input$BodySize){selnames=c(selnames, 2)}
+    if(input$DepthRange){selnames=c(selnames, 3)}
+    if(input$PLD){selnames=c(selnames, 4)}
+    if(input$Diet){selnames=c(selnames, 5)}
+    if(input$Aggregation){selnames=c(selnames, 6)}
+    if(input$Position){selnames=c(selnames, 7)}
+    if(input$ParentalMode){selnames=c(selnames, 8)}
+    
+    fishtrait_sub<-fishtrait[which(rowSums(fishtrait[,seldat])>0),]
+
+    md=fishtrait_sub[,selnames]
     return(md)
   })
   
@@ -85,7 +62,7 @@ shinyServer(function(input, output) {
   
   phyly= reactive({
     FDisClus_phy = as.phylo(clusters())
-    labelClust = as.character (row.names(fishtrait2))
+    labelClust = as.character (row.names(mydata()))
     FDisClus_phy$tip.label = labelClust
     return(FDisClus_phy)
   })
