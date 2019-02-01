27a1be47580465060d334cf2c78f35fd2e7f7565
lark-gorilla <mark.gr.miller@gmail.com>
2019-02-01 15:34:32 +0000

    AT best tree stage. Need to see if can get consensus tree running by 'majority' method. Also 2-norm value still too high for best tree - see if this imporoved once data is cleaned a bit in relation to combining some classes and potentially removing outliers.

--- prelim_paper_analyses.R
+++ prelim_paper_analyses.R
@@ -5,7 +5,9 @@
 library(cluster)
 library(clue)
 
-# read in data
+#################### data clean and distance matrix calculation ##########################
+##########################################################################################
+
 dat<-read.csv('C:/coral_fish/data/Traits/JPN_AUS_RMI_trait_master.csv', h=T)
 
 dat<-dat[which(dat$AUS_sp>0),] # we will focus on Australia for this prelim
@@ -20,36 +22,48 @@ dat[dat$Species=='Manta birostris',]$BodySize<-450
 
 dist1<-daisy(dat[,3:9])
 
-# test different algorithums and make consensus tree
+############### test different algorithums and make consensus tree #######################
+##########################################################################################
 
 hclust_methods <-
   c("ward.D", "ward.D2", "single", "complete", "average", "mcquitty") # median and centroid dont work
 hclust_results <- lapply(hclust_methods, function(m) hclust(dist1, m))
 names(hclust_results) <- hclust_methods 
+
 ## Now create an ensemble from the results.
 hens <- cl_ensemble(list = hclust_results)
-hens
 
 ## Plotting.
-plot(hens, main = names(hens))
-## And continue to analyze the ensemble, e.g.
-tree_agg<-cl_agreement(hens, dist1, method = "cophenetic") # this just calcs cophenetic corr 
-tree_agg
-# between deondrograms - but there are lots of different methods
+#plot(hens, main = names(hens))
 
-lapply(hens, function(x){cor(cophenetic(x), dist1)})
-# also between each other
-cl_agreement(hens, method = "cophenetic")
-# or disagreement
-cl_dissimilarity(hens, method = "cophenetic")
+## see how well the trees represent the original distance matrix.
+## method=spectral is the 2-norm method proposed by Merigot et al. (2010)
+tree_agg<-cl_dissimilarity(hens, dist1, method = "spectral")
+tree_agg
+
+## get threshold 2-norm value for dendrgram to accurately represent dist. 
+## code from within Mouchet et al. 2008 function: ("http://villeger.sebastien.free.fr/R%20scripts/GFD_matcomm.R")
+
+# best tree as hclust object
+btree<-hclust(dist1, method='average')
+sigma2 <- var(dist1) + var(cl_ultrametric(btree))
+threshold <- 2*sqrt(sigma2)*sqrt(dim(dat)[1])
+threshold # still too high
+
+# This is the consensus tree calc from Mouchet - I wouldnt get to work
+# cl_consensus(hens, method="majority", weights=1, control=list(p=1))
+
 # get consensus tree
-constree<-cl_consensus(hens, 'manhattan',control=list(verbose=T))
+constree<-cl_consensus(hens[5:6], 'manhattan',control=list(verbose=T))
 # add to list
 hens[[7]]<-constree
-# not better than all
-hens
 # try weighting by agreement
-constree2<-cl_consensus(hens[1:6], weights = as.vector(tree_agg))
-hens[[8]]<-constree2#
-cl_agreement(hens, dist1, method = "cophenetic") # better but not by much
-plot(hens,main = names(hens))
+constree2<-cl_consensus(hens[5:6], 'manhattan', weights = as.vector(tree_agg)[5:6])
+hens[[8]]<-constree2
+cl_dissimilarity(hens, dist1, method = "spectral")
+# both worse
+
+## we take average algorithum tree forward as the best
+btree<-hclust(dist1, method='average')
+
+
