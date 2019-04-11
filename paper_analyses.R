977a6331d40e0026b770794f077978f58069b557
lark-gorilla <mark.gr.miller@gmail.com>
2019-04-11 20:41:05 +0100

    start of actual paper analyses

---
+++ paper_analyses.R
@@ -0,0 +1,73 @@
+## 11/04/19 
+## fish trait cluster paper analyses
+
+rm(list=ls())
+library(cluster)
+library(clue)
+library(fpc)
+library(ggplot2)
+library(dendextend)
+library(circlize)
+library(vegan)
+library(caret)
+
+# source clVal function
+source('C:/coral_fish/scripts/coral_fish/clVal.R')
+
+
+#################### data clean ##########################
+##########################################################
+
+dat<-read.csv('C:/coral_fish/data/Traits/JPN_AUS_RMI_CHK_MLD_TMR_trait_master_opt2.csv', h=T)
+# Running on simplist classification of Position trait
+
+# Do we want to include sp. in the analyses? - yes.
+# dat[grep('\\.', dat$Species),]
+
+dat<-dat[which(dat$AUS_sp>0 | dat$JPN_sp>0),] # we will focus on Australia and Japan for this prelim
+
+# remove functional duplicates
+dup_trait<-paste(dat$BodySize, dat$DepthRange, dat$PLD, dat$Diet, dat$Aggregation, dat$Position, 
+                 dat$ParentalMode)
+dat<-dat[-which(duplicated(dup_trait)),]
+dat$Species<-as.character(dat$Species)
+dat[which(dat$Species=='Scarus psittacus'),]$Species<-'Scarus psittacus/spinus' # edit for one
+
+# Including those that default to duplicates via NA after gower dist
+dat<-dat[-which(dat$Species=='Caesio sp.'),]
+dat<-dat[-which(dat$Species=='Choerodon cyanodus'),]
+dat<-dat[-which(dat$Species=='Ostracion immaculatus'),]
+
+
+row.names(dat)<-dat$Species
+
+
+## Edit to some trait values from MB 25/10/18
+#dat[dat$Species=='Brotula multibarbata',]$DepthRange<-219
+dat[dat$Species=='Mobula birostris',]$BodySize<-450
+dat[dat$Species=='Amphiprion sandaracinos',]$BodySize<-14
+
+## set ceiling for numeric variables for scaling purposes 04/03/19
+dat[dat$PLD>=100 & !is.na(dat$PLD),]$PLD<-100
+dat[dat$DepthRange>=200 & !is.na(dat$DepthRange),]$DepthRange<-200
+
+#################### run cluster validation function ##########################
+###############################################################################
+
+# analyses on Aus + Jpn combined
+
+#trial to see difference between jaccad and distance measure
+jac_trial<-clVal(data=dat[,3:9], runs=50, max_cl=20, cluster_match = 'jaccard')
+dist_trial<-clVal(data=dat[,3:9], runs=50, max_cl=20, cluster_match = 'distance')
+
+jac_trial$stats$method='jac'
+dist_trial$stats$method='dist'
+
+outy<-rbind(jac_trial$stats, dist_trial$stats)
+
+library(GGally)
+ggpairs(outy, columns = 3:7, ggplot2::aes(colour=method))
+
+
+dat<-dat[which(dat$AUS_sp>0 | dat$JPN_sp>0),]
+
