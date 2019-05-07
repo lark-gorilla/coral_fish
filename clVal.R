c4d8462c36dc273e8894b13f1badfde8626757a4
lark-gorilla <mark.gr.miller@gmail.com>
2019-05-07 13:54:25 +0100

    Changes to swap 0:max_cl loop to run inside 1:1000 iteration loop (rather than vice versa before) This means that the same subsampled data is used to compare each n cluster level = less bias. Some changes made to format of output data also made

--- clVal.R
+++ clVal.R
@@ -1,4 +1,4 @@
-clVal<-function(data=data, runs=10,  max_cl=20, subs_perc=0.95, fast.k.h=0.3)
+clVal<-function(data=data, runs=10, min_cl=3,  max_cl=20, subs_perc=0.95, fast.k.h=0.3)
  
 {
   require(cluster)
@@ -7,31 +7,35 @@ clVal<-function(data=data, runs=10,  max_cl=20, subs_perc=0.95, fast.k.h=0.3)
   
   dist1<-daisy(data, metric='gower', stand = FALSE)
   btree<-hclust(dist1, method='average')
+  if(is.null(btree$labels)){btree$labels<-1:nrow(data)} # edit to make sure tree has labels
   
-  wigl_list<-list()
-  sens_list<-list()
-  spec_list<-list()
+  
+  #create output objects
+  wigl_list<-as.list(rep(list(matrix(data=NA, nrow=nrow(data), ncol=runs,
+                                     dimnames=list(row.names(data), 1:runs))), max_cl))
   jacc_list<-list()
+  for(i in 1:max_cl)
+  {jacc_list[[i]]<-matrix(data=NA, nrow=i, ncol=runs, 
+                   dimnames=list(paste('clust', 1:i, sep=''), 1:runs))}
   
+  jacc_out<-expand.grid(k=min_cl:max_cl, runs=1:runs)
   out_centres<-NULL
-  stats_out<-NULL
-  for(o in 3:max_cl)
-  {
-    kval=o
-    bcut<-cutree(btree, k=kval)
-    
-    wigl_out<-matrix(data=NA, nrow=nrow(data), ncol=runs, dimnames=list(row.names(data), 1:runs))
-    clust_jacc<-matrix(data=NA, nrow=kval, ncol=runs, dimnames=list(paste('clust', 1:kval, sep=''), 1:runs))
-    
-    out_metrics<-data.frame(k=o, runs=1:runs, rnd=NA, jac=NA, wig=NA, sil=NA)
+  stats_out<-expand.grid(k=min_cl:max_cl, runs=1:runs, rnd=NA, jac=NA, wig=NA, sil=NA)
+  
 
-    for ( i in 1:runs)
-    {
-      #resample original distance matrix
-      dist1_matx<-as.matrix(dist1)
-      sub_index<-sort(sample(1:nrow(data), (nrow(data)*subs_perc)))
-      distsub<-dist1_matx[sub_index, sub_index]
-      distsub<-as.dist(distsub)
+
+for ( i in 1:runs)
+{
+  #resample original distance matrix
+  dist1_matx<-as.matrix(dist1)
+  sub_index<-sort(sample(1:nrow(data), (nrow(data)*subs_perc)))
+  distsub<-dist1_matx[sub_index, sub_index]
+  distsub<-as.dist(distsub)
+  
+      for(kval in min_cl:max_cl)
+      {
+      bcut<-cutree(btree, k=kval)
+        
       #make subsample tree and cut 
       subtree<-hclust(distsub, method='average')
       subcut<-cutree(subtree, k=kval) # cut using k value
@@ -41,10 +45,10 @@ clVal<-function(data=data, runs=10,  max_cl=20, subs_perc=0.95, fast.k.h=0.3)
       # Used in fpc::clusterboot
       
       fpc_btree<-disthclustCBI(dist1,  method="average", 
-                               cut="number", k=o)
+                               cut="number", k=kval)
       
       fpc_subtree<-disthclustCBI(distsub,  method="average", 
-                                 cut="number", k=o)
+                                 cut="number", k=kval)
       
       #empty matrix
       jc2<-matrix(data=NA, nrow=kval, ncol=kval,
@@ -64,10 +68,10 @@ clVal<-function(data=data, runs=10,  max_cl=20, subs_perc=0.95, fast.k.h=0.3)
       jc_match<-apply(jc2, 1, function(x){which.max(x)}) 
       jc_match2<-apply(jc2, 2, function(x){which.max(x)})
       
-      clust_jacc[,i]<-apply(jc2, 2, max) # gives the jaccard similarity of each original cluster to
+      jacc_list[[kval]][,i]<-apply(jc2, 2, max) # gives the jaccard similarity of each original cluster to
       # the MOST similar cluster in the resampled data
       
-      out_metrics[i,]$jac<-mean(apply(jc2, 2, max))
+      stats_out[stats_out$k==kval & stats_out$runs==i,]$jac<-mean(apply(jc2, 2, max))
       
       # loop to create cluster centres
       
@@ -77,7 +81,7 @@ clVal<-function(data=data, runs=10,  max_cl=20, subs_perc=0.95, fast.k.h=0.3)
       {
         clust_sp<-subdata[which(subcut==h),]
         my_out<-do.call('c', lapply(as.list(clust_sp), function(x){if(is.factor(x)) {table(x)}else{median(x, na.rm=T)}}))
-        out2<-data.frame(run=i, kval=o, cluster= h, as.list(my_out))
+        out2<-data.frame(run=i, kval=kval, cluster= h, as.list(my_out))
         clust_cent<-rbind(clust_cent, out2)
       }  
       clust_cent$jc_match<-jc_match
@@ -96,7 +100,7 @@ clVal<-function(data=data, runs=10,  max_cl=20, subs_perc=0.95, fast.k.h=0.3)
       ARI <- (a - (a + b) * (a + c)/(a + b + c + d))/((a + b + 
                    a + c)/2 - (a + b) * (a + c)/(a + b + c + d))
       
-      out_metrics[i,]$rnd<- ARI
+      stats_out[stats_out$k==kval & stats_out$runs==i,]$rnd<- ARI
       
       # get h val from k val
       # hacked function from dendextend function heights_per_k.dendrogram()
@@ -114,7 +118,7 @@ clVal<-function(data=data, runs=10,  max_cl=20, subs_perc=0.95, fast.k.h=0.3)
       cutval<-heights_to_cut_by[names(heights_to_cut_by)==kval]
       
       if(length(cutval)==0){
-        print(paste('looking for', o, 'clusters in dendrogram went below fast cutoff search limit, set lower value for fast.k.h', sep=' '))
+        print(paste('looking for', kval, 'clusters in dendrogram went below fast cutoff search limit, set lower value for fast.k.h', sep=' '))
         break}
       
       # copo distance per species
@@ -132,22 +136,18 @@ clVal<-function(data=data, runs=10,  max_cl=20, subs_perc=0.95, fast.k.h=0.3)
         cl_cop_mv<-sp2_copod[which(dimnames(sp2_copod)[[1]] %in% full_n_clust), jc_match2[k]] 
         # using jc2_match2 here lines up original cluster with most similar subs cluster 
         
-        wigl_out[which(dimnames(wigl_out)[[1]] %in% names(cl_cop_mv)),i]<-cl_cop_mv
+        wigl_list[[kval]][which(dimnames(wigl_list[[kval]])[[1]] %in% names(cl_cop_mv)),i]<-cl_cop_mv
         # allows for 5% species dropped to remain NA
       } 
       
-      out_metrics[i,]$wig<- mean(wigl_out[,i], na.rm=T)
+      stats_out[stats_out$k==kval & stats_out$runs==i,]$wig<- mean(wigl_list[[kval]][,i], na.rm=T)
   
-      out_metrics[i,]$sil<-cluster.stats(distsub, subcut)$avg.silwidth
+      stats_out[stats_out$k==kval & stats_out$runs==i,]$sil<-cluster.stats(distsub, subcut)$avg.silwidth
       
-      print(o)
-      
-      wigl_list[[o]]<-wigl_out
-      jacc_list[[o]]<-clust_jacc
-      
-    }
-    stats_out<-rbind(stats_out, out_metrics)
-  }
+      }#close kval loop
+print(i)
+print(Sys.time())
+}# close runs loop
   
 all_out<-list(n_runs=runs, stats=stats_out, clust_centres=out_centres, wiggle=wigl_list, jaccard=jacc_list)
 return(all_out)
