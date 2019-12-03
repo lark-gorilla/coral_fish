80a0f5bd59b061b0010153802c864557c287a218
lark-gorilla <mark.gr.miller@gmail.com>
2019-12-03 16:55:46 +0000

    Running with updated dataset and fix replicating larval_development as sexual_system columns

--- coral_clus_trial.R
+++ coral_clus_trial.R
@@ -13,20 +13,22 @@ library(readxl)
 source('C:/coral_fish/scripts/coral_fish/clVal.R')
 source('C:/coral_fish/scripts/coral_fish/functions.R')
 
-dat<-read_xlsx('C:/coral_fish/data/Traits/Coral_DB_condensed_KMC0107.xlsx',
-                sheet = 2, na='NA')
+#dat<-read_xlsx('C:/coral_fish/data/Traits/Coral_DB_condensed_KMC0107.xlsx',
+#sheet = 2, na='NA')
+
+dat<-read.csv('C:/coral_fish/data/Traits/coral_traits_filter2911.csv')
 
 summary(dat) # 23 missing vals in max depth and reporduction
 str(dat)
 
-dat$Corallite.width.maximum<-as.numeric(dat$Corallite.width.maximum)
-dat$Depth.lower<-as.numeric(dat$Depth.lower)
-dat$growth_rate<-as.numeric(dat$growth_rate)
+#dat$Corallite.width.maximum<-as.numeric(dat$Corallite.width.maximum)
+#dat$Depth.lower<-as.numeric(dat$Depth.lower)
+#dat$growth_rate<-as.numeric(dat$growth_rate)
 
 table(dat$Coloniality)
 table(dat$Corallite.width.maximum)
 table(dat$Depth.lower)
-table(dat$'Growth.form.typical (Brig revised)')
+table(dat$'Growth.form.typical')
 table(dat$growth_rate)
 table(dat$larval_development)
 table(dat$Sexual_system)
@@ -45,13 +47,17 @@ dat$Water.clarity.preference<-factor(dat$Water.clarity.preference,
 # apparently daisy won't accept characters? change to factor
 
 dat$Coloniality<-factor(dat$Coloniality, exclude='NA')
-dat$'Growth.form.typical (Brig revised)'<-factor(dat$'Growth.form.typical (Brig revised)', exclude='NA')
+dat$Growth.form.typical<-factor(dat$Growth.form.typical, exclude='NA')
 dat$larval_development<-factor(dat$larval_development, exclude='NA')
-dat$Sexual_system<-factor(dat$larval_development, exclude='NA')
+dat$Sexual_system<-factor(dat$Sexual_system, exclude='NA')
 
 str(dat)
 
-crl_out<-clVal(data=dat[,2:10], runs=1000,
+crl_out<-clVal(data=dat[,c('Coloniality', 'Corallite.width.maximum',
+                           'Depth.lower','Growth.form.typical',
+                            'larval_development',
+                           'Sexual_system','Wave.exposure.preference',
+                           'Water.clarity.preference')], runs=100,
                min_cl=2, max_cl=20, subs_perc=0.95,
                fast.k.h = 0.1, calc_wigl = F)
 
@@ -71,13 +77,17 @@ ggplot()+
   geom_line(data=a_sum, aes(x=k, y=median), color='green')+
   scale_x_continuous(breaks=2:20)+
   facet_wrap(~variable, scales='free_y')+
-  geom_vline(xintercept = 4, color='cyan')
+  geom_vline(xintercept = 3, color='cyan')
 
-alg_out$clust_centres<-alg_out$clust_centres %>% group_by(kval, jc_match) %>%
+crl_out$clust_centres<-crl_out$clust_centres %>% group_by(kval, jc_match) %>%
   mutate_if(is.numeric, funs(replace(., is.na(.), mean(., na.rm=T)))) %>%
   as.data.frame()
 
-alg_pca<-pca_vis(rundat=dat[,8:17], clValresult=alg_out$clust_centres, kval=7)
+alg_pca<-pca_vis(rundat=na.omit(dat[,c('Coloniality', 'Corallite.width.maximum',
+                               'Depth.lower','Growth.form.typical',
+                               'growth_rate', 'larval_development',
+                               'Sexual_system','Wave.exposure.preference',
+                               'Water.clarity.preference')]), clValresult=crl_out$clust_centres, kval=3)
 
 grid.arrange(alg_pca[[4]], alg_pca[[5]], alg_pca[[6]])
 
@@ -87,8 +97,12 @@ plot(hclust(daisy(dat[,2:10],
                   metric='gower', stand = FALSE), method='average'))
 
 
-full_crl_clust<-cutree(hclust(daisy(dat[,2:10],
-                      metric='gower', stand = FALSE), method='average'), k=4)
+full_crl_clust<-cutree(hclust(daisy(dat[,c('Coloniality', 'Corallite.width.maximum',
+                                           'Depth.lower','Growth.form.typical',
+                                           'growth_rate', 'larval_development',
+                                           'Sexual_system','Wave.exposure.preference',
+                                           'Water.clarity.preference')],
+                      metric='gower', stand = FALSE), method='average'), k=3)
 
 dat$group<-full_crl_clust
 
