4e82fcf8542b701846f0099d4cf0727d40874ec0
lark-gorilla <mark.gr.miller@gmail.com>
2020-03-10 12:01:42 +0000

    Rerun based on Japan only data

--- echinoderm_clus_trial.R
+++ echinoderm_clus_trial.R
@@ -12,7 +12,7 @@ library(readxl)
 # source clVal function
 source('C:/coral_fish/scripts/coral_fish/clVal.R')
 
-dat<-read.csv('C:/coral_fish/data/Traits/echino_traits_2502_clean.csv')
+dat<-read.csv('C:/coral_fish/data/Traits/echino_traitDB_jpn.csv')
 
 summary(dat) # 23 missing vals in max depth and reporduction
 str(dat)
@@ -35,15 +35,6 @@ dat$Exposure<-factor(dat$Exposure,
                           levels=c("Protected", "both","Exposed"), ordered = T,
                           exclude='NA')
 
-
-
-# apparently daisy won't accept characters? change to factor
-
-dat$Coloniality<-factor(dat$Coloniality, exclude='NA')
-dat$Growth.form.typical<-factor(dat$Growth.form.typical, exclude='NA')
-dat$larval_development<-factor(dat$larval_development, exclude='NA')
-dat$Sexual_system<-factor(dat$Sexual_system, exclude='NA')
-
 str(dat)
 
 table(dat$Spines)
@@ -63,7 +54,7 @@ crl_out<-clVal(data=dat[,c('Spines', 'Max_Length','Depth_Range',
                daisytypelist = list(symm=c(1,4,9)),
                runs=500, min_cl=2, max_cl=20, subs_perc=0.95,
                fast.k.h = 0.1, calc_wigl = F, daisyweights=rep(1, 9))
-
+  
 
 a_melt<-melt(crl_out$stats, id.vars=c( 'k', 'runs'))
 a_sum<-a_melt%>%group_by(k, variable)%>%
@@ -80,9 +71,8 @@ ggplot()+
   geom_line(data=a_sum, aes(x=k, y=median), color='green')+
   scale_x_continuous(breaks=2:20)+
   facet_wrap(~variable, scales='free_y')+
-  geom_vline(xintercept = 3, color='cyan')+
-  geom_vline(xintercept = 11, color='cyan')+
-  geom_vline(xintercept = 15, color='cyan')
+  geom_vline(xintercept = 8, color='cyan')+
+  geom_vline(xintercept = 12, color='cyan')
 
 
 # write clusters out
@@ -94,13 +84,12 @@ hc<-hclust(daisy(dat[,c('Spines', 'Max_Length','Depth_Range',
                   metric='gower', 
                  type = list(symm=c(1,4,9)),stand = FALSE), method='average')
 
-plot(hc); rect.hclust(hc, k=3);rect.hclust(hc, k=11, border=4);rect.hclust(hc, k=15, border=3)
+plot(hc); rect.hclust(hc, k=8);rect.hclust(hc, k=12, border=4)
 
 
-dat$groupk3<-cutree(hc, k=3)
-dat$groupk11<-cutree(hc, k=11)
-dat$groupk15<-cutree(hc, k=15)
+dat$groupk8<-cutree(hc, k=8)
+dat$groupk12<-cutree(hc, k=12)
 
 
-write.csv(dat, 'C:/coral_fish/outputs/echinoderm_clust.csv', quote=F, row.names=F)
+write.csv(dat, 'C:/coral_fish/outputs/echinoderm_clust_jpn.csv', quote=F, row.names=F)
 
