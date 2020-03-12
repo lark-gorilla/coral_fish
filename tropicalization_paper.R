9875563b8c6340580c01bb799e72cebbd20b44d9
lark-gorilla <mark.gr.miller@gmail.com>
2020-03-12 21:04:01 +0000

    Outputting biomass by thermal affinity by FG over lat plots (fig 3)

--- tropicalization_paper.R
+++ tropicalization_paper.R
@@ -216,11 +216,12 @@ sg_lat_spans<-data.frame(xmin=c(24.2, 26.2, 28.5, 31,   32.7,  33.38, 34.6),
 
 bio_jpn$ThermalAffinity2<-factor(bio_jpn$ThermalAffinity2, levels=c('tropical', 'subtropical'))
 
-ggplot(data=filter(bio_jpn, FG %in% c(15, 10, 8, 2, 6))) + 
-  geom_rect(data=sg_lat_spans, aes(ymin=0, ymax=5, xmin=xmin, xmax=xmax), fill='grey', alpha=0.5)+
-  geom_point(aes(x = lat, y = (tot_biom/totMsurv)^0.25, colour=ThermalAffinity2), shape=1)+
+p1<-ggplot(data=filter(bio_jpn, FG %in% c(15, 10, 8, 2, 6, 12, 4, 1, 16))) + 
+  geom_rect(data=sg_lat_spans, aes(ymin=0, ymax=4, xmin=xmin, xmax=xmax), fill='grey', alpha=0.5)+
+  geom_point(data=filter(bio_jpn, FG %in% c(15, 10, 8, 2, 6, 12, 4, 1, 16) & ((tot_biom/totMsurv)^0.25)<4),
+             aes(x = lat, y = (tot_biom/totMsurv)^0.25, colour=ThermalAffinity2), shape=1)+
   geom_smooth(aes(x = lat, y = (tot_biom/totMsurv)^0.25, colour=ThermalAffinity2),se=F)+
-  facet_wrap(~FG, scales='free', nrow=5)+
+  facet_grid(FG~., scales='free_y')+
   theme_bw()+theme(legend.position = "none")+
   xlab('Latitude')+ylab('4rt-trans standardised biomass')
 
@@ -229,15 +230,17 @@ sg_lat_spans<-data.frame(xmin=c(-23.4, -24.8, -26.61, -28.19, -29.9, -29.97),
 
 bio_aus$ThermalAffinity2<-factor(bio_aus$ThermalAffinity2, levels=c('tropical', 'subtropical'))
 
-ggplot(data=filter(bio_aus, FG %in% c(15, 10, 8, 2, 6))) + 
-    geom_rect(data=sg_lat_spans, aes(ymin=0, ymax=5, xmin=xmin, xmax=xmax), fill='grey', alpha=0.5)+
-    geom_point(aes(x = Lat, y = (tot_biom/totMsurv)^0.25, colour=ThermalAffinity2), shape=1)+
+p2<-ggplot(data=filter(bio_aus, FG %in% c(15, 10, 8, 2, 6, 12, 4, 1, 16))) + 
+    geom_rect(data=sg_lat_spans, aes(ymin=0, ymax=4, xmin=xmin, xmax=xmax), fill='grey', alpha=0.5)+
+    geom_point(data=filter(bio_aus, FG %in% c(15, 10, 8, 2, 6, 12, 4, 1, 16) & ((tot_biom/totMsurv)^0.25)<4),
+               aes(x = Lat, y = (tot_biom/totMsurv)^0.25, colour=ThermalAffinity2), shape=1)+
     geom_smooth(aes(x = Lat, y = (tot_biom/totMsurv)^0.25, colour=ThermalAffinity2),se=F)+
-    facet_wrap(~FG, scales='free', nrow=5)+scale_x_reverse()+
+    facet_grid(FG~., scales='free_y')+scale_x_reverse()+
     theme_bw()+theme(legend.position = "none")+
     xlab('Latitude')+ylab('4rt-trans standardised biomass')
   
-
+grid.arrange(p1, p2, ncol=2)
+#plots have outlier points removed (>4) but curves still fitter to full data
 # make same plot with spprich data while we're here
 
 ggplot(filter(spr_jpn, FG %in% c(15, 10, 8, 2, 6, 12, 4, 1, 16)),
